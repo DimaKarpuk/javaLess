3d8295bc80c494bd0b0c6301a80727cb860eb941
Dzmitry Karpuk <dzmitry.karpuk@corp.itibo.com>
2024-11-27 15:20:38 +0300

    giti

--- jenkins.groovy
+++ jenkins.groovy
@@ -1,23 +1,6 @@
-task_branch = "${TEST_BRANCH_NAME}"
-def branch_cutted = task_branch.contains("origin") ? task_branch.split('/')[1] : task_branch.trim()
-currentBuild.displayName = "$branch_cutted"
-base_git_url = "https://github.com/DimaKarpuk/javaLess.git"
-
 
 node {
-    withEnv(["branch=${branch_cutted}", "base_url=${base_git_url}"]) {
-        stage("Checkout Branch") {
-            if (!"$branch_cutted".contains("master")) {
-                try {
-                    getProject("$base_git_url", "$branch_cutted")
-                } catch (err) {
-                    echo "Failed get branch $branch_cutted"
-                    throw ("${err}")
-                }
-            } else {
-                echo "Current branch is master"
-            }
-        }
+    withEnv() {
             stage ("Allure") {
                 generateAllure()
         }
@@ -26,21 +9,7 @@ node {
 
 
 def runTestWithTag() {
-    try {
         labelledShell(label: "Run", script: "chmod +x gradlew \n./gradlew -x test")
-    } finally {
-        echo "some failed tests"
-    }
-}
-
-def getProject(String repo, String branch) {
-    cleanWs()
-    checkout scm: [
-            $class           : 'GitSCM', branches: [[name: branch]],
-            userRemoteConfigs: [[
-                                        url: repo
-                                ]]
-    ]
 }
 
 def generateAllure() {
