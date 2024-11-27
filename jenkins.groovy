91a0e2acb366fbd8fc1231d41b05b1a5a3f7d023
Dzmitry Karpuk <dzmitry.karpuk@corp.itibo.com>
2024-11-27 15:22:17 +0300

    giti

--- jenkins.groovy
+++ jenkins.groovy
@@ -1,15 +1,28 @@
-
 node {
-    withEnv() {
-            stage ("Allure") {
-                generateAllure()
+    withEnv([]) {
+        stage("Checkout") {
+            checkout scm
+        }
+
+        stage("Build") {
+            sh './gradlew build'
+        }
+
+        stage("Test") {
+            runTestWithTag()
+        }
+
+        stage("Allure") {
+            generateAllure()
         }
     }
 }
 
-
 def runTestWithTag() {
-        labelledShell(label: "Run", script: "chmod +x gradlew \n./gradlew -x test")
+    labelledShell(label: "Run", script: """
+        chmod +x gradlew
+        ./gradlew test
+    """)
 }
 
 def generateAllure() {
