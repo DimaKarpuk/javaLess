cfff458c417d1f0d17b4edd0e09a0d8926f04b9b
Dzmitry Karpuk <dzmitry.karpuk@corp.itibo.com>
2024-11-28 13:24:28 +0300

    delete

---
+++ src/test/java/colections/SimpleJavaTest.java
@@ -0,0 +1,14 @@
+package colections;
+
+import org.junit.jupiter.api.Test;
+
+public class SimpleJavaTest {
+
+    @Test
+    public void testJava(){
+        int a =10;
+        int b = 11;
+
+        simpleMyCollections
+    }
+}
